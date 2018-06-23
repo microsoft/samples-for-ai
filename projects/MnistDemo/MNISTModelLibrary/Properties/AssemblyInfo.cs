d7193a79b95c3c217dd3623e49f030a782b01738
Yanji Chen <v-yanch@microsoft.com>
2018-06-23 13:04:51 +0800

    Modified Assembly Information

--- projects/MnistDemo/MNISTModelLibrary/Properties/AssemblyInfo.cs
+++ projects/MnistDemo/MNISTModelLibrary/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("MNISTModelLibrary")]
-[assembly: AssemblyCopyright("Copyright © OpenMindStudio, Microsoft 2018")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
