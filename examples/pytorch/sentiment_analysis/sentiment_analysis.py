7b5b0acc606da2e98ddf5c04482105964ecae76e
Chi Cheng Min <chec@microsoft.com>
2018-07-10 00:53:28 +0800

    Updates

--- examples/pytorch/sentiment_analysis/sentiment_analysis.py
+++ examples/pytorch/sentiment_analysis/sentiment_analysis.py
@@ -1,4 +1,24 @@
-﻿import os
+﻿# ====================================================================================================== #
+# The MIT License (MIT)
+# Copyright (c) Microsoft Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial
+# portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ====================================================================================================== #
+
+import os
 import argparse
 import re
 import numpy as np
@@ -114,8 +134,8 @@ def train(args):
         train_corrects = preds.eq(batch.label.squeeze()).sum().item()
 
         numTrain += len(batch)
-        print('Epoch: {:.2f} {} / {} \tLoss: {:.4f} Acc: {:.2f}% ({} / {})'
-            .format(numTrain / len(train_iter.dataset), numTrain, len(train_iter.dataset), loss.item(), 
+        print('Epoch: {:.2f} \tLoss: {:.4f} Acc: {:.2f}% ({} / {})'
+            .format(numTrain / len(train_iter.dataset), loss.item(),
             100. * train_corrects / len(batch), train_corrects, len(batch)), end='\r')
 
         if nIteration % 200 == 0:
